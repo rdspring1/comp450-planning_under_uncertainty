f2cd390c5bfdcc2633d30ef3ae320552a882be2a
Ryan Spring <rdspring1@gmail.com>
2014-11-02 17:31:06 -0600

    Implemented Policy Execution

--- SMR.cpp
+++ SMR.cpp
@@ -1,36 +1,36 @@
 /*********************************************************************
-* Software License Agreement (BSD License)
-*
-*  Copyright (c) 2008, Willow Garage, Inc.
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-*
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of the Willow Garage nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*********************************************************************/
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
 
 /* Author: Ioan Sucan */
 
@@ -41,6 +41,7 @@
 #include "ompl/control/spaces/DiscreteControlSpace.h"
 #include <limits>
 #include <math.h>
+#include <memory>
 
 ompl::control::SMR::SMR(const SpaceInformationPtr &si) : base::Planner(si, "SMR")
 {
@@ -151,16 +152,18 @@ double ompl::control::SMR::ps(int id, base::State* state, base::Goal* goal)
 
 ompl::base::PlannerStatus ompl::control::SMR::solve(const base::PlannerTerminationCondition &ptc)
 {
-    //TODO Generate Policy using SMR if new start/goal
+    // Generate Policy using SMR if new start/goal
     checkValidity();
     base::Goal                   *goal = pdef_->getGoal().get();
 
+    std::vector<int> startStates;
     int startSize = 0;
     while (const base::State *st = pis_.nextStart())
     {
         Motion *motion = new Motion(siC_, nn_->size());
         si_->copyState(motion->state, st);
         nn_->add(motion);
+        startStates.push_back(nn_->size());
         ++startSize;
     }
 
@@ -190,7 +193,54 @@ ompl::base::PlannerStatus ompl::control::SMR::solve(const base::PlannerTerminati
             }
         }
     }
-    //TODO Use Policy to create a path between start/goal; Return automatically if an obstacle is encountered
+
+    // Use Policy to create a path between start/goal; Return automatically if an obstacle is encountered
+    /* set the solution path */
+    bool solved = false;
+    Motion* motion = motions[startStates.front()];
+    Motion* result = NULL;
+    PathControl *path = new PathControl(si_);
+    while(!ptc)
+    {
+        // Select Action based on Current State
+        int action = 0;
+        double max_value = 0;
+        for(auto& value : smrtable[motion->id_])
+        {
+            if(value.second > max_value)
+            {
+                max_value = value.second;
+                action = value.first;
+            }
+        }
+
+        // Propagate (CurrentState, Action, Steps, NextState)
+        Control* control = siC_->allocControl();
+        control->as<DiscreteControlSpace::ControlType>()->value = action;
+
+        result = new Motion();
+        int steps = siC_->propagateWhileValid(motion->state, control, siC_->getMinControlDuration(), result->state);
+        
+        if(steps != siC_->getMinControlDuration())
+        {
+            break;
+        }
+        else if(goal->isSatisfied(result->state))
+        {
+            solved = true;
+            break;
+        }
+        else
+        {
+            path->append(motion->state, control, siC_->getMinControlDuration() * siC_->getPropagationStepSize());
+            motion = nn_->nearest(result);
+        }
+    }
+
+    path->append(result->state);
+    pdef_->addSolutionPath(base::PathPtr(path));
+
+    return base::PlannerStatus(solved, false);
 }
 
 void ompl::control::SMR::getPlannerData(base::PlannerData &data) const
